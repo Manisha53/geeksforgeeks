1c98478477033abcdf2d24cf2619c6aa33644f1a
Manisha Pandey <37845282+Manisha53@users.noreply.github.com>
2019-09-02 20:17:13 +0530

    Create Count nodes of linked list.java

---
+++ Count nodes of linked list.java
@@ -0,0 +1,63 @@
+{
+import java.util.*;
+class Node{
+    int data;
+    Node next;
+    
+    Node(int a){
+        data = a;
+        next = null;
+    }
+    
+}
+class LinkedList{
+    
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            Node head=null;
+            for(int i=0;i<n;i++)
+            {int a=sc.nextInt();
+            if(head==null){
+                head=new Node(a);
+            }
+            else
+                insert(head,a);
+            }
+            CountNodes g = new CountNodes();
+            System.out.println(g.getCount(head));
+        }
+    }
+    public static Node insert(Node head,int a){
+        if(head==null){
+            return new Node(a);
+        }
+        head.next=insert(head.next,a);
+        return head;
+    }
+}
+}
+/*This is a function problem.You only need to complete the function given below*/
+/*Complete the function below*/
+/*
+class Node{
+    int data;
+    Node next;
+    Node(int a){  data = a; next = null; }
+}*/
+class CountNodes{
+    public int getCount(Node head){
+        
+        //Code here
+        int c=0;
+        Node temp=head;
+        while(temp!=null){
+            c++;
+            temp=temp.next;
+        }
+        return c;
+    }
+    
+}
