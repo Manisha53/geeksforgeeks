2a6cbaec47960e9b5d62d269bed4dee66e07e636
Manisha Pandey <37845282+Manisha53@users.noreply.github.com>
2019-09-02 16:27:42 +0530

    Create Sum of BigIntegers.java

---
+++ Sum of BigIntegers.java
@@ -0,0 +1,46 @@
+{
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+//Position this line where user code will be pasted.
+// Driver class with main function
+class GFG {
+	
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int testcase = sc.nextInt();
+		
+		while(testcase-- > 0){
+		    // Taking first element 
+		    // in BigInteger x
+		    BigInteger x = sc.nextBigInteger();
+		    
+		    // Taking second element in
+		    // BigInteger y
+		    BigInteger y = sc.nextBigInteger();
+		    
+		    // Calling function add to add two BigIntegers
+		    MathematicalOperation obj = new MathematicalOperation();
+		    System.out.println(obj.add(x, y));
+		}
+		
+		
+	}
+}
+}
+//------------------------------------
+
+class MathematicalOperation{
+    
+    static BigInteger add(BigInteger x, BigInteger y){
+            
+        // Your code here
+        BigInteger sum=x.add(y);
+        
+        return sum;
+        
+    }
+    
+}
