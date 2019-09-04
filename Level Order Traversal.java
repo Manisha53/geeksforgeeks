353c20758ce93e4fdf934e2f08f38a4fb4e75fcf
Manisha Pandey <37845282+Manisha53@users.noreply.github.com>
2019-09-04 11:03:32 +0530

    Create Level Order Traversal.java

---
+++ Level Order Traversal.java
@@ -0,0 +1,106 @@
+{
+// INITIAL CODE
+import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.HashMap;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+// A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+class Drivercode
+{
+	
+	
+    // driver function to test the above functions
+    public static void main(String args[])
+    {
+		
+        // Input the number of test cases you want to run
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+		//Node root=null;
+        while (t > 0)
+        {
+            HashMap<Integer, Node> m = new HashMap<Integer, Node> ();
+            int n = sc.nextInt();
+            Node root=null;
+            while (n > 0)
+            {
+                int n1 = sc.nextInt();
+                int n2 = sc.nextInt();
+                char lr = sc.next().charAt(0);
+                //  cout << n1 << " " << n2 << " " << (char)lr << endl;
+                Node parent = m.get(n1);
+                if (parent == null)
+                {
+                    parent = new Node(n1);
+                    m.put(n1, parent);
+                    if (root == null)
+                        root = parent;
+                }
+                Node child = new Node(n2);
+                if (lr == 'L')
+                    parent.left = child;
+                else
+                    parent.right = child;
+                m.put(n2, child);
+                n--;
+            }
+		
+            Level_order_traversal g = new Level_order_traversal();
+            g.levelOrder(root);
+			System.out.println();
+            t--;
+			
+        }
+    }
+}
+
+}
+/*This is a function problem.You only need to complete the function given below*/
+/*
+class Node
+{
+    int data;
+    Node left, right;
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+/-----------------------------------------------my code---------------------------
+class Level_order_traversal
+{
+    //You are required to complete this method
+    static void levelOrder(Node node) 
+    {
+        // Your code here
+        Queue<Node> q=new LinkedList<Node>();
+        q.add(node);
+        while(q.size()!=0){
+            Node t=q.poll();     //returns element at head
+            int x=t.data;
+            System.out.print(x+" ");
+            if(t.left!=null){
+                q.add(t.left);
+            }
+            if(t.right!=null){
+                q.add(t.right);
+            }
+        }
+    }
+}
