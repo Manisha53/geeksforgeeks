804f733942b034d84f90d669eee18f106e3bd258
Manisha Pandey <37845282+Manisha53@users.noreply.github.com>
2019-10-07 20:19:27 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,30 @@
+/*This is a function problem.You only need to complete the function given below*/
+//User function Template for Java
+// Function to check if number is even or odd
+class Geeks {
+    // 
+	 static void evenOdd (int a,int b) 
+	 {
+		//Your code here
+		if(a%2==0){
+		    System.out.println(a);
+		    System.out.println(b);
+		}
+		else{
+		    System.out.println(b);
+		    System.out.println(a);
+		}
+	 }
+}
+
+
+/*
+Example:
+Input:
+1
+4 5
+
+Output:
+4
+5
+*/
