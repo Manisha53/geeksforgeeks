3fc4bec148e341068ac4ed6452c706bddaa55f1a
Manisha Pandey <37845282+Manisha53@users.noreply.github.com>
2019-10-07 21:15:59 +0530

    Create whileLoop_printTable.java

---
+++ whileLoop_printTable.java
@@ -0,0 +1,23 @@
+class Geeks {
+	 static void printTable (int n) 
+	 {
+            int multiplier=10;
+            while(multiplier>0)
+            {
+                //Your code here
+                System.out.print((multiplier--)*n+" ");
+            }
+       System.out.println();
+	 }
+}
+
+/*
+Input:
+2
+1
+2
+
+Output:
+10 9 8 7 6 5 4 3 2 1
+20 18 16 14 12 10 8 6 4 2
+*/
