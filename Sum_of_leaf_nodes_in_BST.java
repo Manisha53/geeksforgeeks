d2a6fcd09288ec88701edc8da9cead35e6c04024
Manisha Pandey <37845282+Manisha53@users.noreply.github.com>
2019-11-07 01:26:34 +0530

    Create Sum_of_leaf_nodes_in_BST.java

---
+++ Sum_of_leaf_nodes_in_BST.java
@@ -0,0 +1,24 @@
+/*
+class Node
+{
+    int data;
+    Node left, right;
+    Node(int key)
+    {
+        data = key;
+        left = right = null;
+    }
+}*/
+class GfG
+{
+    public static int sumOfLeafNodes(Node root)
+    {
+        if(root == null)
+          return 0;
+          
+        if(root.left == null && root.right == null)
+          return root.data;
+          
+        return sumOfLeafNodes(root.left)+sumOfLeafNodes(root.right);
+    }
+}
